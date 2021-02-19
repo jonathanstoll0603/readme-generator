d9b78980d1ae900d547bea3ad4efde33bbb6c66f
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-02-18 23:52:56 -0500

    placed inquirer function inside init function for initialization, and placed markdown template inside of the generateMarkdown folder for better organization

--- index.js
+++ index.js
@@ -3,84 +3,85 @@ const inquirer = require("inquirer");
 const { prompt } = require("inquirer");
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
-// const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
+console.log(generateMarkdown)
 
 // TODO: Create a function to write README file
-const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is your github username?',
-            name: 'github'
-        },
-        {
-            type: 'input',
-            message: 'What is your email address?',
-            name: 'email'
-        },
-        {
-            type: 'input',
-            message: 'What is the name of your project?',
-            name: 'projectName'
-        },
-        {
-            type: 'input',
-            message: 'Please write a short description of your project.',
-            name: 'description'
-        },
-        {
-            type: 'list',
-            message: 'What type of liscense should your project include?',
-            name: 'license',
-            choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"]
-        },
-        {
-            type: 'input',
-            message: 'What command should be used to install dependencies?',
-            name: 'install'
-            // npm i
-        },
-        {
-            type: 'input',
-            message: 'What command should be used to run test?',
-            name: 'test'
-            // npm test
-        },
-        {
-            type: 'input',
-            message: 'What does the user need to know about using the repository?',
-            name: 'use'
-            // npm test
-        },
-        {
-            type: 'input',
-            message: 'What does the user need to know about contributing to the repository?',
-            name: 'contribution'
-            // npm test
-        }
-    ])
-}
+function init() {
+    const promptUser = () => {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What is your github username?',
+                name: 'github'
+            },
+            {
+                type: 'input',
+                message: 'What is your email address?',
+                name: 'email'
+            },
+            {
+                type: 'input',
+                message: 'What is the name of your project?',
+                name: 'projectName'
+            },
+            {
+                type: 'input',
+                message: 'Please write a short description of your project.',
+                name: 'description'
+            },
+            {
+                type: 'list',
+                message: 'What type of liscense should your project include?',
+                name: 'license',
+                choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"]
+            },
+            {
+                type: 'input',
+                message: 'What command should be used to install dependencies?',
+                name: 'install'
+                // npm i
+            },
+            {
+                type: 'input',
+                message: 'What command should be used to run test?',
+                name: 'test'
+                // npm test
+            },
+            {
+                type: 'input',
+                message: 'What does the user need to know about using the repository?',
+                name: 'use'
+                // npm test
+            },
+            {
+                type: 'input',
+                message: 'What does the user need to know about contributing to the repository?',
+                name: 'contribution'
+                // npm test
+            }
+        ])
+    }
 
-const readme = ({ github, email, projectName, description, license, install, test, use, contribution}) => {
-    return ` 
-    #${projectName}
-
+    promptUser().then((response) => {
+        const readmeTemplate = generateMarkdown(response)
     
-    `
-}
-
-promptUser().then((response) => {
-    const readmeTemplate = readme(response)
-
-    writeFileAsync("READMEexample.md", readmeTemplate)
-    .then(() => {
-        console.log("Generating README....")
+        writeFileAsync("READMEexample.md", readmeTemplate)
+        .then(() => {
+            console.log("Generating README....")
+        })
+        .catch(err => console.error(err));
+        
     })
-    .catch(err => console.error(err));
+}
+
+// const generateMarkdown = ({ github, email, projectName, description, license, install, test, use, contribution}) => {
+//     return ` 
+//     #${projectName}
+
     
-})
+//     `
+// }
 
 // Function call to initialize app
-function init() {}
-
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,90 +1,108 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = (license) => {
+    if (license == "none") {
+        return;
+    } else {
+    renderLicenseLink(license);
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseLink = (license) => {
+    if (license == "none") {
+        return;
+    } else if (license == "MIT") {
+        return `https://img.shields.io/badge/License-MIT-yellow.svg`
+    } else if (license == "Apache 2.0") {
+        return `https://img.shields.io/badge/License-Apache%202.0-blue.svg`
+    } else if (license == "GPL 3.0") {
+        return `https://img.shields.io/badge/License-GPLv3-blue.svg`
+    } else if (license == "BSD 3") {
+        return `https://img.shields.io/badge/License-BSD%203--Clause-blue.svg`
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `
-  # ${projectName}
+const generateMarkdown = ({ github, email, projectName, description, license, install, test, use, contribution}) => {
+    return `
+    # ${projectName}
 
-  ![GitHub License](url to selected license)
-  
-  ---
-  
-  ## Description
-  
-  ${description}
-  
-  ---
-  
-  ## Table of Contents
-  
-  * [Installation](#installation)
-  
-  * [Usage](#usage)
-  
-  * [License](#license)
-  
-  * [Contribution](#contribution)
-  
-  * [Tests](#tests)
-  
-  * [Questions](#questions)
-  
-  ---
-  
-  ## Installation
-  To install the necessary dependancies run the following command:
-  
-  \`\`\`\
-  ${install}
-  \`\`\`\
-  ---
-  
-  
-  ## Usage
-  
-  ${use}
-  
-  ---
-  
-  ## License
-  
-  This project is licensed under the ${license} license.
-  
-  ---
-  
-  ## Contributions
-  
-  ${contribution}
-  
-  ---
-  
-  ## Tests
-  
-  To run tests, run the following command:
-  
-  \`\`\`\
-  ${test}
-  \`\`\`\
-  
-  ---
-  
-  ## Questions
-  
-  For any questions, please contact us via email at ${email}, or GitHub at ![Github Account](${github}).
-  
-  ---   
+    ![GitHub License](` + renderLicenseLink({license}) + `)
+    
+    ---
+    
+    ## Description
+    
+    ${description}
+    
+    ---
+    
+    ## Table of Contents
+    
+    * [Installation](#installation)
+    
+    * [Usage](#usage)
+    
+    * [License](#license)
+    
+    * [Contribution](#contribution)
+    
+    * [Tests](#tests)
+    
+    * [Questions](#questions)
+    
+    ---
+    
+    ## Installation
+    To install the necessary dependancies run the following command:
+    
+    \`\`\`\
+    ${install}
+    \`\`\`\
+    ---
+    
+    
+    ## Usage
+    
+    ${use}
+    
+    ---
+    
+    ## License
+    
+    This project is licensed under the ${license} license.
+    
+    ---
+    
+    ## Contributions
+    
+    ${contribution}
+    
+    ---
+    
+    ## Tests
+    
+    To run tests, run the following command:
+    
+    \`\`\`\
+    ${test}
+    \`\`\`\
+    
+    ---
+    
+    ## Questions
+    
+    For any questions, please contact us via email at ${email}, or GitHub at ![Github Account](${github}).
+    
+    ---   
 
-`;
+    `;
 }
 
 module.exports = generateMarkdown;
